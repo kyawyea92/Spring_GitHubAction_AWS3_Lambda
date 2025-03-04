ccb4ded973febf45ac4d07371d8f9b3754339eb4
Kyaw Ye Lwin <kyawyealwin2017@gmail.com>
2025-03-04 14:18:28 +0700

    updated pom file

--- Spring_AWS3_Lambda/src/main/java/com/aws/lambda/controller/StudentController.java
+++ Spring_AWS3_Lambda/src/main/java/com/aws/lambda/controller/StudentController.java
@@ -5,10 +5,7 @@ import com.aws.lambda.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,7 +17,7 @@ public class StudentController {
     public StudentService studentService;
 
     @PostMapping
-    public ResponseEntity<StudentDTO> createStudent(StudentDTO studentDTO) {
+    public ResponseEntity<StudentDTO> createStudent(@RequestBody StudentDTO studentDTO) {
         // Add business logic here to create a student
         studentService.addStudent(studentDTO);
         return new ResponseEntity<>(studentDTO, HttpStatus.OK);
